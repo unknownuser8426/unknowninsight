bd33048e755192bf9d88956f3a74947f7bac032a
unknownuser8426 <daniel.cairney.djc@gmail.com>
2025-08-25 00:11:20 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,128 +1,95 @@
-function loadFile(filePath) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
-  xmlhttp.send();
-  if (xmlhttp.status==200) {
-    result = xmlhttp.responseText;
-  }
-  return result;
-}
+let essayArray = [];
+let analysisArray = [];
+let reviewsArray = [];
 
-let body = loadFile('./poem.txt');
-
-console.log(body);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const fs = require('fs');
-const promisifiedReadfile = require('./promisifiedReadfile');
-
-fs.readFile('./poem.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  let firstSentence = data;
-  fs.readFile('./file2.txt',  'utf-8', (err, data) => {
-    if (err) throw err;
-    let secondSentence = data;
-    console.log(firstSentence, secondSentence);
-  });
-});
- */
-
-
-
-
-
-
-
-
-
-
-
-/* const verseChoose = document.querySelector("select");
-const poemDisplay = document.querySelector("pre");
-
-verseChoose.addEventListener("change", () => {
-  const verse = verseChoose.value;
-  updateDisplay(verse);
-});
-
-function updateDisplay(verse) {
-    verse = verse.replace(" ", "").toLowerCase();
-    const url = `${verse}.txt`;
-}
-
-fetch(url)
-    .then(response => {
-    if (!response.ok) {
-        throw new Error(`Could not fetch ${url}`);
+class writing {
+    constructor(path, type, title, subtitle, dateData, metaInfo){
+        this._path = path;
+        this._type = type;
+        this._title = title;
+        this._subtitle = subtitle;
+        this._dateData = dateData;
+        this._metaInfo = metaInfo
     }
-    return response.text();
-    })
-    .then(text => {
-    poemDisplay.textContent = text;
-    })
-    .catch(error => {
-    poemDisplay.textContent = "Error loading poem.";
-    console.error(error);
-    });
+}
+
+class essay extends writing {
+    constructor(path, type, title, subtitle, dateData, metaInfo, photoLink, firstLines){
+        super(path, type, title, subtitle, dateData, metaInfo);
+        this._length = length;
+        this._photoLink = photoLink;
+        this._firstLines = firstLines;
+        essayArray.push(this);
+    }
+}
+
+class analysis extends writing {
+    constructor(path, type, title, subtitle, dateData, metaInfo, firstLines){
+        super(path, type, title, subtitle, dateData, metaInfo);
+        this._firstLines = firstLines;
+        analysisArray.push(this);
+    }
+}
+
+class reviews extends writing {
+    constructor(path, type, title, subtitle, dateData, metaInfo, stars){
+        super(path, type, title, subtitle, dateData, metaInfo);
+        this._stars= stars;
+        reviewsArray.push(this);
+    }
+}
 
 
- */
+let writings = [
+new essay("./files/Essays/Essay1.html", "essay", "Essay 1", "The essay yall have been waiting for", [2025, 4, 28], '28th April 2025 | 3 Hours', '<img src="https://www.nurseryworld.co.uk/media/0aipyhc5/f779371ec117aac5711bd52f0138d4fb.jpg?width=1002&height=668&bgcolor=White&v=1da4d833cc7c2f0"/>', "My central argument is that, not only is Rousseau's 'forced to be free' not contradictory"), 
+new essay("./files/Essays/Essay1.html", "essay", "Essay 2", "The essay yall have been waiting for", [2025, 4, 28], '28th April 2025 | 3 Hours', '<img src="https://www.nurseryworld.co.uk/media/0aipyhc5/f779371ec117aac5711bd52f0138d4fb.jpg?width=1002&height=668&bgcolor=White&v=1da4d833cc7c2f0"/>', "My central argument is that, not only is Rousseau's 'forced to be free' not contradictory"), 
+new essay("./files/Essays/Essay1.html", "essay", "Essay 3", "The essay yall have been waiting for", [2025, 4, 28], '28th April 2025 | 3 Hours', '<img src="https://www.nurseryworld.co.uk/media/0aipyhc5/f779371ec117aac5711bd52f0138d4fb.jpg?width=1002&height=668&bgcolor=White&v=1da4d833cc7c2f0"/>', "My central argument is that, not only is Rousseau's 'forced to be free' not contradictory"), 
+new essay("./files/Essays/Essay1.html", "essay", "Essay 4", "The essay yall have been waiting for", [2025, 4, 28], '28th April 2025 | 3 Hours', '<img src="https://www.nurseryworld.co.uk/media/0aipyhc5/f779371ec117aac5711bd52f0138d4fb.jpg?width=1002&height=668&bgcolor=White&v=1da4d833cc7c2f0"/>', "My central argument is that, not only is Rousseau's 'forced to be free' not contradictory"),
+new essay("./files/Essays/Essay1.html", "essay", "Essay 5", "The essay yall have been waiting for", [2025, 4, 28], '28th April 2025 | 3 Hours', '<img src="https://www.nurseryworld.co.uk/media/0aipyhc5/f779371ec117aac5711bd52f0138d4fb.jpg?width=1002&height=668&bgcolor=White&v=1da4d833cc7c2f0"/>', "My central argument is that, not only is Rousseau's 'forced to be free' not contradictory"),
+new essay("./files/Essays/Essay1.html", "essay", "Essay 6", "The essay yall have been waiting for", [2025, 4, 28], '28th April 2025 | 3 Hours', '<img src="https://www.nurseryworld.co.uk/media/0aipyhc5/f779371ec117aac5711bd52f0138d4fb.jpg?width=1002&height=668&bgcolor=White&v=1da4d833cc7c2f0"/>', "My central argument is that, not only is Rousseau's 'forced to be free' not contradictory"),
 
-/* function load() {
-    var file = new XMLHttpRequest();
-    file.open("GET", "http://remote.tld/random.txt", true);
-    file.onreadystatechange = function() {
-      if (file.readyState === 4) {  // Makes sure the document is ready to parse
-        if (file.status === 200) {  // Makes sure it's found the file
-          text = file.responseText;
-          document.getElementById("div1").innerHTML = text;
+new reviews("path", "review1", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new reviews("path", "review2", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new reviews("path", "review3", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new reviews("path", "review4", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new reviews("path", "review5", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+
+new analysis("path", "analysis1", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new analysis("path", "analysis2", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new analysis("path", "analysis3", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new analysis("path", "analysis4", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+new analysis("path", "analysis5", "Review 1", "remove substitile", "19th", "2 hours aho", 4),
+
+
+
+ ];
+
+let masterArray = [];
+masterArray.push(essayArray);
+masterArray.push(analysisArray);
+masterArray.push(reviewsArray);
+
+
+console.log(masterArray);
+
+
+let j = 5;
+let i = 0;
+
+window.onload = function(){
+    while (i<3){
+        while (j>0){
+            document.getElementById(`title${i}${j}`).innerHTML = masterArray[i][j]._title;
+            document.getElementById(`subtitle${i}${j}`).innerHTML = masterArray[i][j]._subtitle;
+            document.getElementById(`metaInfo${i}${j}`).innerHTML = masterArray[i][j]._metaInfo;
+            j--;
+            console.log(j);
         }
-      }
+        i++;
+        j = 4;
+        console.log(i);
     }
-}
-
-window.onLoad = load();
-
-const fs = require('fs');
-
-      
-Here we use fs.readfile() and callback functions:
-
-
-// Here we use native promises with our "promisified" version of readfile:
-let firstSentence;
-promisifiedReadfile('./file.txt', 'utf-8')
-  .then((data) => {
-    firstSentence = data;
-    return promisifiedReadfile('./file2.txt', 'utf-8');
-  })
-  .then((data) => {
-    let secondSentence = data;
-    console.log(firstSentence, secondSentence)
-  })
-  .catch((err) => {console.log(err)});
- */
-// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
-/* async function readFiles() {
-  let firstSentence = await promisifiedReadfile('./Essays/Essay1.txt', 'utf-8');
-  let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8');
-  console.log(firstSentence, secondSentence);
-}
-
-console.log(readFiles()); */
+    document.getElementById('photoLink1').innerHTML = essayArray[i]._photoLink;
+};
 
 
